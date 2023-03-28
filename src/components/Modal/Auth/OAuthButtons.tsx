2e3f3896684c4ec030f6dc1e348a13537d0a595a
Danish <danishpinto102@gmail.com>
2023-03-28 20:13:24 +0530

    complete index page

---
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -0,0 +1,39 @@
+import { Flex, Button, Image, Text } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../../../firebase/clientApp";
+
+const OAuthButtons: React.FC = () => {
+  const [SignInWithGoogle, userCred, loading, error] =
+    useSignInWithGoogle(auth);
+
+  const createUserDocument = async (user: User) => {
+    const userDocRef = doc(firestore, "users", user.uid);
+    await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+  };
+
+  useEffect(() => {
+    if (userCred) {
+      createUserDocument(userCred.user);
+    }
+  }, [userCred]);
+
+  return (
+    <Flex direction="column" width="100%" mb={4}>
+      <Button
+        variant="oauth"
+        mb={2}
+        isLoading={loading}
+        onClick={() => SignInWithGoogle()}
+      >
+        <Image src="/images/googlelogo.png" height="20px" mr={4} />
+        Continue with Google
+      </Button>
+      <Button variant="oauth">Some Other Provider</Button>
+      {error && <Text>{error.message}</Text>}
+    </Flex>
+  );
+};
+export default OAuthButtons;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { theme } from "../chakra/theme";
+// import { Component } from 'react';
+import Layout from "../components/Layout/Layout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
+  );
 }
+
+// function MyApp({ Component: pageProps }: AppProps) {
+//   return (
+//     <ChakraProvider>
+//       <Component {...pageProps} />
+//     </ChakraProvider>
+//   );
+// }
+
+// export default MyApp;
