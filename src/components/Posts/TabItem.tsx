42cab74d301547babf085dcd49d54f0378513390
Danish <danishpinto102@gmail.com>
2023-03-30 23:28:04 +0530

    Fixed bugs

--- src/components/Posts/TabItem.tsx
+++ src/components/Posts/TabItem.tsx
@@ -1,9 +1,9 @@
 import { Flex, Icon, Text } from "@chakra-ui/react";
 import React from "react";
-import { TabItem } from "./NewPostForm";
+import { TabIte } from "./NewPostForm";
 
 type TabItemProps = {
-  item: TabItem;
+  item: TabIte;
   selected: boolean;
   setSelectedTab: (value: string) => void;
 };
